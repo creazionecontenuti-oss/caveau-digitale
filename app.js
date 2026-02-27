b4306eed93fdb555812d4ca40cc734bd25aa901e
André Wagner Vitiani <creazionecontenuti@gmail.com>
2026-02-27 19:02:42 +0100

    fix: Transak window.open invece di iframe (X-Frame-Options)

--- app.js
+++ app.js
@@ -345,8 +345,7 @@ App.openTransak = function() {
   url.searchParams.set('themeColor', '3b82f6');
   url.searchParams.set('disableWalletAddressForm', 'true');
   if (state.address) url.searchParams.set('walletAddress', state.address);
-  document.getElementById('transak-iframe').src = url.toString();
-  App.openModal('modal-transak');
+  window.open(url.toString(), '_blank', 'width=450,height=700,noopener,noreferrer');
 };
 
 function renderDashboard() {
